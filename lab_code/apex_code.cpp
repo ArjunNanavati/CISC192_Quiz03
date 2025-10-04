1b448cfa24aefe76b0bb235e4f0e854b2d8c550e
ArjunNanavati <tootsie.nanavati@gmail.com>
2025-10-03 17:06:31 -0700

    apex_code.cpp

--- lab_code/apex_code.cpp
+++ lab_code/apex_code.cpp
@@ -7,20 +7,22 @@ using namespace std;
 
 void quiz(stringstream& cin, stringstream& cout)
 {
-    // Prompt user to enter a line (no punctuation)
-    // Output the line but with the first word moved to the end of the line with adjusting the capitalization and
-    // additional mark.
-
-    // Hint: Deal with the capitalization the very last!
-    // Hint: You might need to use the following methods:
-    //   - string::find()
-    //   - string::at()
-    //   - string::append()
-    //   - string::insert()
-    //   - string::length()
-    //   - string::substr()
-    //   - tolower()
-    //   - toupper()
+string userLine;
+   getline(cin, userLine);
     
+    int spaceIndex = userLine.find(" ");
+    
+    string firstWord = userLine.substr(0, spaceIndex);
+    
+    string rest = userLine.substr(spaceIndex + 1);
+    
+    rest.at(0) = toupper(rest.at(0));
+
+    firstWord.at(0) = tolower(firstWord.at(0));
+     
+    string result = rest + " " + firstWord + "?";
+
+    cout << result << endl;
 }
+
 
